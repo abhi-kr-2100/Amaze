f58282b7c05d0ffebaa1f0b357994d296d00f41e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-08-09 16:19:54 +0530

    Render the default maze
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- app/(tabs)/maze.tsx
+++ app/(tabs)/maze.tsx
@@ -1,9 +1,50 @@
-import { View, Text } from "react-native";
+import { useAppSelector } from "@/hooks/redux";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function MazeScreen() {
+  const rectangles = useAppSelector((state) => state.maze.rectangles);
+
   return (
-    <View>
-      <Text>Maze</Text>
+    <View style={styles.maze}>
+      {rectangles.map((row, r) => (
+        <View style={styles.mazeRow} key={r}>
+          {row.map((cell, c) => (
+            <View style={styles.mazeRect} key={c}></View>
+          ))}
+        </View>
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  maze: {
+    flex: 1,
+
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+    gap: 5,
+  },
+  mazeRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+
+    gap: 5,
+  },
+  mazeRect: {
+    flex: 1,
+    width: 10,
+    height: 10,
+
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+
+    backgroundColor: "black",
+  },
+});
