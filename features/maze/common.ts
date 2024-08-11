17e9c399d5864ed05c116b03806b5526561c0586
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-08-11 13:14:29 +0530

    Add utility to convert between coords and keys
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- features/maze/common.ts
+++ features/maze/common.ts
@@ -1,1 +1,11 @@
 export type Coord2D = [number, number];
+
+const RC_SEP = "|";
+
+export function CoordToKey([r, c]: Coord2D) {
+  return `${r}${RC_SEP}${c}`;
+}
+
+export function KeyToCoord(key: string): Coord2D {
+  return key.split(RC_SEP).map(Number) as Coord2D;
+}
